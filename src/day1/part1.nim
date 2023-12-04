c223437661b7ff446f8600af9b157d13b5baac25
Graham Drakeley <gt.drakeley@gmail.com>
2023-12-04 13:09:46 -0800

    day1

---
+++ src/day1/part1.nim
@@ -0,0 +1,42 @@
+import std/[os, sequtils, strutils]
+
+
+const questionRoot = currentSourcePath.parentDir
+const inputFilename = "input.txt"
+const inputFilepath = questionRoot.joinPath(inputFilename)
+
+
+proc forwardScan(line: string): char =
+  for ch in line:
+    if ch.isDigit:
+      return ch
+
+
+iterator reverse[T](iter: openArray[T]): T =
+  for idx in countup(1, iter.len):
+    yield iter[^idx]
+
+
+proc backwardScan(line: string): char =
+  for ch in line.reverse:
+    if ch.isDigit:
+      return ch
+
+
+proc intFromLine(line: string): int =
+  let firstDigit = forwardScan(line)
+  let lastDigit = backwardScan(line)
+  parseInt(firstDigit & lastDigit)
+
+
+proc main =
+  let infile = open(inputFilepath, fmRead)
+  var nums: seq[int]
+  for line in infile.lines:
+    nums.add line.intFromLine
+  infile.close()
+  echo nums.foldl(a + b)
+
+
+when isMainModule:
+  main()
---
+++ src/day1/part2.nim
@@ -0,0 +1,79 @@
+import std/[os, sequtils, strutils, sugar, tables]
+
+
+const questionRoot = currentSourcePath.parentDir
+const inputFilename = "input.txt"
+const inputFilepath = questionRoot.joinPath(inputFilename)
+const intWordCharTable = toOrderedTable(
+  {
+    "zero": '0',
+    "one": '1',
+    "two": '2',
+    "three": '3',
+    "four": '4',
+    "five": '5',
+    "six": '6',
+    "seven": '7',
+    "eight": '8',
+    "nine": '9'
+  }
+)
+
+
+type LocValue = object
+  idx: int
+  ch: char
+
+
+proc newLocValue(idx: int, ch: char): LocValue = LocValue(idx: idx, ch: ch)
+proc `<`(l, r: LocValue): bool = l.idx < r.idx
+
+
+proc getFirstDigitCharLoc(line: string): LocValue =
+  for idx, ch in line:
+    if ch.isDigit:
+      return newLocValue(idx, ch)
+
+
+proc getLastDigitCharLoc(line: string): LocValue =
+  for idx in countup(1, line.len):
+    if line[^idx].isDigit:
+      return newLocValue(line.len - idx, line[^idx])
+
+
+proc getFirstDigit(line: string): char =
+  let firstDigitCharLoc = line.getFirstDigitCharLoc
+  let digitWordLocs = collect:
+    for word, ch in intWordCharTable.pairs:
+      let idx = line.find(word)
+      if idx != -1:
+        newLocValue(idx, ch)
+
+  min(@[firstDigitCharLoc] & digitWordLocs).ch
+
+
+proc getLastDigit(line: string): char =
+  let lastDigitCharLoc = line.getLastDigitCharLoc
+  let digitWordLocs = collect:
+    for word, ch in intWordCharTable.pairs:
+      let idx = line.rfind(word)
+      if idx != -1:
+        newLocValue(idx, ch)
+
+  max(@[lastDigitCharLoc] & digitWordLocs).ch
+
+
+proc getCalibrationValueFromLine(line: string): int = parseInt(getFirstDigit(line) & getLastDigit(line))
+
+
+proc main =
+  let infile = open(inputFilepath, fmRead)
+  var nums: seq[int]
+  for line in infile.lines:
+    nums.add line.getCalibrationValueFromLine
+  infile.close()
+  echo nums.foldl(a + b)
+
+
+when isMainModule:
+  main()
